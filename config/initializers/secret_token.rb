1764e24c9dd039622f4f880bdbea039ec6dba6c8
Aimee Maher <aimee@geekycheeks.com>
2014-02-05 22:25:30 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GmailToText::Application.config.secret_token = '1bec1e6b0c1eab7c9c26566361d5d51cc631b8c2081bf7bb6a53072c7a41e78d0dd1d5111b5211e9ab4468db9bd0a152754ec14272db6bf5cda87b5d9351e7c9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GmailToText::Application.config.session_store :cookie_store, key: '_gmail_to_text_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GmailToText::Application.config.session_store :active_record_store
